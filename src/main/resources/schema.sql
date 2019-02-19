a36ad510bf6e34a93db66a00c968dcfc18ed3a7e
Jens Schauder <jschauder@pivotal.io>
2019-02-19 15:17:51 +0100

    Application setup works

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table LEGO_MODEL
+(
+  Id   SERIAL primary key,
+  name varchar(200)
+);
---
+++ src/test/java/de/schauderhaft/ddd/jdbc/LegoModelTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.schauderhaft.ddd.jdbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Jens Schauder
+ */
+@RunWith(SpringRunner.class)
+@DataJdbcTest
+public class LegoModelTest {
+
+	@Autowired
+	LegoModels repository;
+
+	@Test
+	public void test() {
+		LegoModel womenOfNasa = new LegoModel("Women of NASA");
+
+		repository.save(womenOfNasa);
+	}
+}
